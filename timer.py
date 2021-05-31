c54e8b2bd45fc436a0ef8e53c9e739d5af43b34c
Avinash <2891786+avinash431@users.noreply.github.com>
2021-05-31 14:21:03 +0800

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,10 @@
+def timer(fn):
+    @wraps(fn)
+    def measure_time(*args, **kwargs):
+        t1 = time.time()
+        result = fn(*args, **kwargs)
+        t2 = time.time()
+        print (
+            "@timer:" + fn.func_name + " took " + str(t2 - t1) + " seconds")
+        return result
+    return measure_time
